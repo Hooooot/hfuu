9a5681bcaba704b6a83616ab6fd1042577d3d570
Hooooot <2446926687@qq.com>
2019-09-22 23:02:27 +0800

    创建dao和service

--- src/main/java/com/hfuu/web/controller/teacher/HomeTController.java
+++ src/main/java/com/hfuu/web/controller/teacher/HomeTController.java
@@ -1,18 +1,26 @@
 package com.hfuu.web.controller.teacher;
 
 
+import com.hfuu.web.entity.TeacherEntity;
+import com.hfuu.web.service.teacher.ITeacherService;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
+
 @Controller
 @RequestMapping("")
 public class HomeTController {
     static private Logger log = Logger.getLogger(HomeTController.class);
+    @Resource(name = "teacherService")
+    private ITeacherService teacherService;
 
     @RequestMapping(value = {"/teacher/home"}, method = RequestMethod.GET)
     public String toHome() {
+        TeacherEntity teacherEntity = null;
+        teacherService.addTeacher(teacherEntity);
         log.debug("跳转到:teacher/home.jsp");
         return "teacher/home";
     }
---
+++ src/main/java/com/hfuu/web/service/teacher/TeacherServiceImpl.java
@@ -0,0 +1,28 @@
+package com.hfuu.web.service.teacher;
+
+import com.hfuu.web.dao.ITeacherDao;
+import com.hfuu.web.entity.TeacherEntity;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service("teacherService")
+public class TeacherServiceImpl implements ITeacherService {
+    static private Logger log = Logger.getLogger(TeacherServiceImpl.class);
+    @Resource()
+    private ITeacherDao dao;
+    @Override
+    public int addTeacher(TeacherEntity teacherEntity) {
+        log.info("teacher service impl");
+        return 0;
+    }
+
+    public ITeacherDao getDao() {
+        return dao;
+    }
+
+    public void setDao(ITeacherDao dao) {
+        this.dao = dao;
+    }
+}
